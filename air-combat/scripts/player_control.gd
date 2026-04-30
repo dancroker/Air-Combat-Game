ec8654f9b5239cf107a3a6d7ca06dc71d8bbe2e0
dancroker <160754622+dancroker@users.noreply.github.com>
2026-04-30 22:05:43 +0100

    Cleaned up code left in player_control
    - A previous unused version of the movement code was left commented out within the file, which has now been removed

--- air-combat/scripts/player_control.gd
+++ air-combat/scripts/player_control.gd
@@ -2,8 +2,8 @@ extends CharacterBody2D
 
 @export var rotation_speed = 1.5
 @export var engine_force = 50
-@export var min_engine_force = 100 /10
-@export var max_engine_force = 300 /10
+@export var min_engine_force = 100 #/10
+@export var max_engine_force = 300 #/10
 @export var engine_change_force = 5
 
 
@@ -28,26 +28,3 @@ func _physics_process(delta):
 	get_input()
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
-#@export var speed = 400
-#@export var engine_force = 50
-#@export var engine_change_force = 10
-#@export var rotation_speed = 1.5
-#
-#var rotation_direction = 0
-#
-#func get_input():
-	#var input_direction = Input.get_axis("up", "down") #,-y , 
-	#rotation_direction = Input.get_axis("left", "right")
-	##velocity = input_direction * speed
-	#if input_direction != 0:
-		#if input_direction > 0:
-				#engine_force -= engine_change_force
-		#else:
-			#engine_force += engine_change_force
-	#velocity.y = engine_force
-	#
-#
-#func _physics_process(delta):
-	#get_input()
-	#rotation += rotation_direction * rotation_speed * delta
-	#move_and_slide()
